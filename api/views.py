062df97ddae53945ce1df214ac143f7bfcacc06c
ellisdeibroncci <alexander.delbrocco@proton.me>
2026-03-10 13:40:06 +0000

    feat: api views get and create functionality

--- api/views.py
+++ api/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Account, Category, Transaction, Budget
+from .serializers import AccountSerializer, CategorySerializer, TransactionSerializer, BudgetSerializer
 
-# Create your views here.
+
+class AccountViewSet(viewsets.ModelViewSet):
+  """Handle account crud operations"""
+
+  serializer_class = AccountSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get_queryset(self):
+    return Account.objects.filter(user=self.request.user)
+
+  def perform_create(self, serializer):
+    serializer.save(user=self.request.user)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+  """Handle category crud operations"""
+
+  serializer_class = CategorySerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get_queryset(self):
+    return Category.objects.filter(user=self.request.user)
+
+  def perform_create(self, serializer):
+    serializer.save(user=self.request.user)
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+  """Handle transaction crud operations"""
+
+  serializer_class = TransactionSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get_queryset(self):
+    return Transaction.objects.filter(account__user=self.request.user)
+
+  def perform_create(self, serializer):
+    serializer.save()
+
+
+class BudgetViewSet(viewsets.ModelViewSet):
+  """Handle budget crud operations"""
+
+  serializer_class = BudgetSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get_queryset(self):
+    return Budget.objects.filter(user=self.request.user)
+
+  def perform_create(self, serializer):
+    serializer.save(user=self.request.user)
