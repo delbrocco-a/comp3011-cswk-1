0b219e7ac689eeeeeaabdcaedb5f5f856c9f0201
ellisdeibroncci <alexander.delbrocco@proton.me>
2026-03-11 10:06:11 +0000

    feat: create rego/login/logout views
    closes: #12

--- api/urls.py
+++ api/urls.py
@@ -5,15 +5,18 @@ from . import views
 
 router = DefaultRouter()
 router.register(
-  r'accounts', views.AccountViewSet, basename='account')
+  r'accounts', views.AccountViewSet, basename="account")
 router.register(
-  r'categories', views.CategoryViewSet, basename='category')
+  r'categories', views.CategoryViewSet, basename="category")
 router.register(
-  r'transactions', views.TransactionViewSet, basename='transaction')
+  r'transactions', views.TransactionViewSet, basename="transaction")
 router.register(
-  r'budgets', views.BudgetViewSet, basename='budget')
+  r'budgets', views.BudgetViewSet, basename="budget")
 
 
 urlpatterns = [
   path('', include(router.urls)),
+  path("auth/register/", views.register, name="register"),
+  path("auth/login/", views.login, name="login"),
+  path("auth/logout/", views.logout, name="logout"),
 ]
